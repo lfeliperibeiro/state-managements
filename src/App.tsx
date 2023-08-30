7b080caf2d4c6d879ed745e2ff7333e4512794af
Felipe Ribeiro <piporibeiro@gmail.com>
2023-08-30 17:10:52 -0300

    feat: init player page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { AddTodo } from './components/AddTodo';
+import './styles/global.css'
+
 import {Provider as ReduxProvider} from 'react-redux'
 import { store } from './store';
+import { Player } from './pages/Player';
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      
+      <Player/>
     </ReduxProvider>
   );
 }
