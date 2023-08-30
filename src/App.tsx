cac4b726f547e0bf4711fcf8ff09720d8a1a981d
Felipe Ribeiro <piporibeiro@gmail.com>
2023-08-30 16:46:38 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { AddTodo } from './components/AddTodo';
+import {Provider as ReduxProvider} from 'react-redux'
+import { store } from './store';
+
+export function App() {
+  return (
+    <ReduxProvider store={store}>
+      
+    </ReduxProvider>
+  );
+}
