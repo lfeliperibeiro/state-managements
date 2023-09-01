507e68497757e006634c02efeb33994157e19d36
Felipe Ribeiro <piporibeiro@gmail.com>
2023-09-01 10:31:21 -0300

    feat: pass modules using redux

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,5 +1,8 @@
+import * as Collapsible from '@radix-ui/react-collapsible';
+
 import { ChevronDown } from 'lucide-react';
 import { Lesson } from './Lesson';
+import { useAppSelector } from '../store';
 
 interface ModuleProps {
   moduleIndex: number;
@@ -7,10 +10,13 @@ interface ModuleProps {
   amountOfLesson: number;
 }
 
-export function Module({moduleIndex, title, amountOfLesson }: ModuleProps) {
+export function Module({ moduleIndex, title, amountOfLesson }: ModuleProps) {
+  const lessons = useAppSelector(
+    (state) => state.player.course.modules[moduleIndex].lessons,
+  );
   return (
-    <div>
-      <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+    <Collapsible.Root className="group">
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
           {moduleIndex + 1}
         </div>
@@ -18,13 +24,21 @@ export function Module({moduleIndex, title, amountOfLesson }: ModuleProps) {
           <strong>{title}</strong>
           <span className="text-xs text-zinc-400">{amountOfLesson} aulas</span>
         </div>
-        <ChevronDown className="h-5 w-5 ml-auto text-zinc-400" />
-      </button>
-      <nav className="relative flex flex-col gap-4 p-6">
-        <Lesson title='Fundamentos do Redux' duration='09:15'/>
-        <Lesson title='Fundamentos do Redux' duration='07:30' />
-        <Lesson title='Fundamentos do Redux' duration='05:30' />
-      </nav>
-    </div>
+        <ChevronDown className="h-5 w-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-6">
+          {lessons.map((lesson) => {
+            return (
+              <Lesson
+                key={lesson.id}
+                title={lesson.title}
+                duration={lesson.duration}
+              />
+            );
+          })}
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
   );
 }
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -2,8 +2,11 @@ import { MessageCircle } from 'lucide-react';
 import { Header } from '../components/Header';
 import { VideoPlayer } from '../components/VideoPlayer';
 import { Module } from '../components/Module';
+import { useAppSelector } from '../store';
 
 export function Player() {
+  const modules = useAppSelector((state) => state.player.course.modules);
+
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1100px] flex-col gap-6">
@@ -19,31 +22,16 @@ export function Player() {
             <VideoPlayer />
           </div>
           <aside className="w-80 absolute top-0 bottom-0 right-0 border-l border-x-zinc-800 divide-y-2 divide-zinc-900 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            <Module
-              moduleIndex={0}
-              title="Desvendando o Redux"
-              amountOfLesson={12}
-            />
-            <Module
-              moduleIndex={1}
-              title="Desvendando o Redux"
-              amountOfLesson={12}
-            />
-            <Module
-              moduleIndex={2}
-              title="Desvendando o Redux"
-              amountOfLesson={12}
-            />
-            <Module
-              moduleIndex={3}
-              title="Desvendando o Redux"
-              amountOfLesson={12}
-            />
-            <Module
-              moduleIndex={4}
-              title="Desvendando o Redux"
-              amountOfLesson={12}
-            />
+            {modules.map((module, index) => {
+              return (
+                <Module
+                  key={module.id}
+                  moduleIndex={index}
+                  title={module.title}
+                  amountOfLesson={module.lessons.length}
+                />
+              );
+            })}
           </aside>
         </main>
       </div>
