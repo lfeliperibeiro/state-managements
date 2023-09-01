b88525d6d7fb33def28829bc404445aeb01140d9
Felipe Ribeiro <piporibeiro@gmail.com>
2023-09-01 16:01:45 -0300

    feat: auto play and change player

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,7 +1,10 @@
 import Player from 'react-player';
 import { useAppSelector } from '../store';
+import { useDispatch } from 'react-redux';
+import { next } from '../store/slices/player';
 
 export function VideoPlayer() {
+  const dispatch = useDispatch()
   const videoLesson = useAppSelector((state) => {
     const { currentModuleIndex, currentLessonIndex } = state.player;
 
@@ -12,6 +15,10 @@ export function VideoPlayer() {
 
     return currentLesson;
   });
+
+  function handlePlayNext(){
+    dispatch(next())
+  }
   
   return (
     <div className="w-full bg-zinc-950 aspect-video">
@@ -19,6 +26,8 @@ export function VideoPlayer() {
         width="100%"
         height="100%"
         controls
+        onEnded={handlePlayNext}
+        playing
         url={`https://www.youtube.com/watch?v=${videoLesson.id}`}
       />
     </div>
